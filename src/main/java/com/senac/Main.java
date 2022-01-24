aa0d316e0e99dee4200241a2d0bb67a9f43f2d99
sala19a <pedroh.mix>
2022-01-24 19:25:01 -0300

    Inserindo ferramenta para ordenar o dado

--- src/main/java/com/senac/Main.java
+++ src/main/java/com/senac/Main.java
@@ -4,23 +4,25 @@ import com.senac.models.Sorteio;
 import com.senac.models.Usuario;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Usuario> jogadores = new ArrayList<>();
 
-        Usuario max = new Usuario("max",35 ,17,16);
-        Usuario matheus = new Usuario("matheus", 9,33,45);
-        Usuario adriely = new Usuario ("adriely",3,14,10);
-        Usuario leo = new Usuario ("Leo o BRABO" , 15 , 11 , 34);
-        Usuario marcio = new Usuario ("Marcio" , 23 , 47 , 37);
-        Usuario alex = new Usuario("Alex", 28,2,50);
-        Usuario bruno = new Usuario("Bruno", 18,42,29);
-        Usuario junin = new Usuario("Formigao", 7, 22,27);
-        Usuario biel = new Usuario("Biel o cara", 44,13,24);
-        Usuario vitu = new Usuario("vitu", 15,5,36);
-        Usuario daniel = new Usuario("Daniel",20,25,30);
+        Usuario max = new Usuario("max", 35, 17, 16);
+        Usuario matheus = new Usuario("matheus", 9, 33, 45);
+        Usuario adriely = new Usuario("adriely", 3, 14, 10);
+        Usuario leo = new Usuario("Leo o BRABO", 15, 11, 34);
+        Usuario marcio = new Usuario("Marcio", 23, 47, 37);
+        Usuario alex = new Usuario("Alex", 28, 2, 50);
+        Usuario bruno = new Usuario("Bruno", 18, 42, 29);
+        Usuario junin = new Usuario("Formigao", 7, 22, 27);
+        Usuario biel = new Usuario("Biel o cara", 44, 13, 24);
+        Usuario vitu = new Usuario("vitu", 15, 5, 36);
+        Usuario daniel = new Usuario("Daniel", 20, 25, 30);
 
         jogadores.add(matheus);
         jogadores.add(adriely);
@@ -34,7 +36,14 @@ public class Main {
         jogadores.add(vitu);
         jogadores.add(daniel);
 
+        //ESTE É O MEU PRIMEIRO COMENTÁRIO
+        //TODO EU PRECISO MELHORAR A FORMA COMO CAPTURO ESSES DADOS
         Sorteio sorteio = new Sorteio(jogadores);
+
+        List<Usuario> sortedList = jogadores.stream()
+                .sorted(Comparator.comparingInt(Usuario::getQuantidadeNumerosAcertados)
+                        .reversed())
+                .collect(Collectors.toList());
 
         System.out.println();
 
